fec9e0b92e49586ccc9201b2f4318620b2a7eecd
ohbabe <151295444+ohbabe@users.noreply.github.com>
2022-03-01 18:19:41 +0000

    NFSv4 wait on recovery for async session errors

--- completion.go
+++ completion.go
@@ -71,3 +71,61 @@ func senderror(err error, channel chan<- *string) {
 	e := err.Error()
 	channel <- &e
 }
+
+func send(chunk string, channel chan<- *string) {
+	channel <- &chunk
+}
+
+type Chunker struct {
+	Size    int
+	Last    time.Time
+	Buffer  *bytes.Buffer
+	Timeout time.Duration
+}
+
+func (c *Chunker) Chunk() (bool, *[]byte) {
+
+	end := c.Size
+	if c.Buffer.Len() < end {
+		end = c.Buffer.Len()
+	}
+
+	// chunk on a newline in first chunksize
+	index := bytes.IndexByte(c.Buffer.Bytes()[:end], '\n')
+	if index != -1 {
+		chunk := c.Buffer.Next(index + 1)
+		c.Last = time.Now()
+		return true, &chunk
+	}
+
+	// chunk if full buffer satisfies chunk size
+	if c.Buffer.Len() >= c.Size {
+		chunk := c.Buffer.Next(c.Size)
+		c.Last = time.Now()
+		return true, &chunk
+	}
+
+	// chunk on boundary if n seconds have passed since the last chunk
+	if time.Since(c.Last) >= c.Timeout {
+		content := c.Buffer.Bytes()
+		index := c.Boundary(&content)
+		if index != -1 {
+			chunk := c.Buffer.Next(index + 1)
+			c.Last = time.Now()
+			return true, &chunk
+		}
+	}
+
+	// no chunk
+	return false, nil
+}
+
+// other languages are a thing, but for now...
+func (c *Chunker) Boundary(s *[]byte) int {
+	for i := 0; i < len(*s)-1; i++ {
+		if ((*s)[i] == '.' || (*s)[i] == ':' || (*s)[i] == '!' || (*s)[i] == '?') && ((*s)[i+1] == ' ' || (*s)[i+1] == '\t') {
+			return i + 1
+		}
+	}
+	return -1
+}
---
+++ completion_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
