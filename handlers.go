75add10ce01f9d22a6096430d9fffef746f5384f
ohbabe <151295444+ohbabe@users.noreply.github.com>
2022-04-07 21:27:20 +0000

    heroku-automated-project-analysis (adding libcsv v0.9.1)

--- handlers.go
+++ handlers.go
@@ -113,3 +113,117 @@ func handleSave(ctx *ChatContext) {
 		ctx.Reply(fmt.Sprintf("Error saving configuration: %s", err.Error()))
 		return
 	}
+
+	ctx.Reply(fmt.Sprintf("Configuration saved to: %s", filename))
+}
+
+func handleBecome(ctx *ChatContext) {
+
+	if !ctx.IsAdmin() {
+		ctx.Reply("You don't have permission to perform this action.")
+		return
+	}
+
+	tokens := ctx.Args
+	if len(tokens) < 2 {
+		ctx.Reply("Usage: /become <personality>")
+		return
+	}
+
+	personality := tokens[1]
+	if cfg, err := loadPersonality(personality); err != nil {
+		ctx.Reply(fmt.Sprintf("Error loading personality: %s", err.Error()))
+		return
+	} else {
+		vip.MergeConfigMap(cfg.AllSettings())
+		ctx.SetConfig(cfg)
+	}
+	ctx.Session.Reset()
+	log.Printf("changing nick to %s", ctx.Personality.Nick)
+
+	ctx.Client.Cmd.Nick(ctx.Personality.Nick)
+	time.Sleep(2 * time.Second)
+	sendGreeting(ctx)
+}
+
+func handleList(ctx *ChatContext) {
+	personalities := listPersonalities()
+	ctx.Reply(fmt.Sprintf("Available personalities: %s", strings.Join(personalities, ", ")))
+}
+
+func handleLeave(ctx *ChatContext) {
+
+	if !ctx.IsAdmin() {
+		ctx.Reply("You don't have permission to perform this action.")
+		return
+	}
+
+	// sendMessage(c, &e, getChatCompletionString(
+	// 	[]ai.ChatCompletionMessage{
+	// 		{
+	// 			Role:    ai.ChatMessageRoleAssistant,
+	// 			Content: viper.GetString("prompt") + viper.GetString("goodbye"),
+	// 		},
+	// 	},
+	// ))
+
+	log.Println("exiting...")
+	go func() {
+		time.Sleep(1 * time.Second)
+		os.Exit(0)
+	}()
+}
+
+func handleDefault(ctx *ChatContext) {
+	args := ctx.Args
+	msg := strings.Join(args, " ")
+	ctx.Session.Message(ctx, ai.ChatMessageRoleUser, msg)
+	rch := ChatCompletionTask(ctx)
+	reply := spoolFromChannel(ctx, rch)
+	ctx.Session.Message(ctx, ai.ChatMessageRoleAssistant, *reply)
+}
+
+func handleSay(ctx *ChatContext) {
+
+	if !ctx.IsAdmin() {
+		ctx.Reply("You don't have permission to perform this action.")
+		return
+	}
+
+	if len(ctx.Args) < 2 {
+		ctx.Reply("Usage: /say [/as <personality>] <message>")
+		ctx.Reply("Example: /msg chatbot /say /as marvin talk about life")
+		return
+	}
+
+	// if second token is '/as' then third token is a personality
+	// and we should play as that personality
+	as := vip.GetString("become")
+	if len(ctx.Args) > 2 && ctx.Args[1] == "/as" {
+		as = ctx.Args[2]
+		ctx.Args = ctx.Args[2:]
+	}
+
+	if cfg, err := loadPersonality(as); err != nil {
+		ctx.Reply(fmt.Sprintf("Error loading personality: %s", err.Error()))
+		return
+	} else {
+		ctx.SetConfig(cfg)
+	}
+
+	ctx.Session = sessions.Get(uuid.New().String())
+	ctx.Session.Reset()
+	ctx.Event.Params[0] = ctx.Config.Channel
+	ctx.Event.Source.Name = ctx.Personality.Nick
+	ctx.Args = ctx.Args[1:]
+
+	handleDefault(ctx)
+}
+
+func keysAsString(m map[string]string) string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return strings.Join(keys, ", ")
+}
